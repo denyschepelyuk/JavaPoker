fd559108d5c020205bb05b6b04f8f74533bebd70
chepelid <denyschepeliuk@gmail.com>
2025-01-19 16:59:44 +0100

    Implement temporary random driven AI player

---
+++ poker/src/core/AIPlayer.java
@@ -0,0 +1,22 @@
+package core;
+
+public class AIPlayer extends Player {
+    public AIPlayer(String name, int initialChips, Game game) {
+        super(name, initialChips, game);
+    }
+
+    public void makeDecision() {
+        double randomValue = Math.random();
+        if (randomValue < 0.2) {
+            fold();
+        } else if (randomValue < 0.4) {
+            check();
+        } else if (randomValue < 0.6) {
+            check();
+        } else {
+            int randomBet = (int) (Math.random() * getChips()/50);
+            makeBet(randomBet * 50);
+        }
+    }
+
+}
