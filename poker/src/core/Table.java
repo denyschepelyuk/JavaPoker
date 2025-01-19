1b3e699f1a1a59b4f9e83f4f0427c285de1b485c
chepelid <denyschepeliuk@gmail.com>
2025-01-19 16:57:41 +0100

    Implement Table class

--- poker/src/core/Table.java
+++ poker/src/core/Table.java
@@ -1,4 +1,34 @@
 package core;
 
+import java.util.ArrayList;
+
 public class Table {
+    private ArrayList<Card> communityCards;
+    private int pot;
+
+    public Table() {
+        communityCards = new ArrayList<>();
+        pot = 0;
+    }
+
+    public void addCommunityCard(Card card) {
+        communityCards.add(card);
+    }
+
+    public void resetTable() {
+        communityCards.clear();
+        pot = 0;
+    }
+
+    public int getPotAmount() {
+        return pot;
+    }
+
+    public void addBet(int amount) {
+        pot += amount;
+    }
+
+    public ArrayList<Card> getCommunityCards() {
+        return communityCards;
+    }
 }
