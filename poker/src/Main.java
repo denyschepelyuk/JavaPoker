cb7e5e55623bb5805c2d42741cc34bff5d31ae71
chepelid <denyschepeliuk@gmail.com>
2025-01-19 17:01:34 +0100

    Update main method

--- poker/src/Main.java
+++ poker/src/Main.java
@@ -1,6 +1,11 @@
 import core.Card;
+import core.Deck;
+import core.Game;
+import core.Player;
 
 public class Main {
     public static void main(String[] args) {
+        Game game = new Game();
+        game.run(3);
     }
 }
