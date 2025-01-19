97fa12954a563cddf2474c29d55362c3198492ed
chepelid <denyschepeliuk@gmail.com>
2025-01-19 15:47:55 +0100

    Implement ui package

--- poker/src/ui/ConsoleUI.java
+++ poker/src/ui/ConsoleUI.java
@@ -1,4 +1,36 @@
 package ui;
 
+import core.Game;
+import core.Player;
+import core.Table;
+import core.Card;
+
 public class ConsoleUI {
+    public void renderGame(Game game) {
+        Table table = game.getTable();
+
+        System.out.println("\n--- Table State ---");
+        System.out.println("Pot: " + table.getPotAmount());
+        System.out.println("Community Cards:");
+
+        for (Card card : table.getCommunityCards()) {
+            System.out.println("  " + card.getDescription());
+        }
+
+        System.out.println("\n--- Players ---");
+        for (Player player : game.getPlayers()) {
+            System.out.println(player.getName() + " [Chips: " + player.getChips() + "]"
+                    + (player.isFolded() ? " (Folded)" : ""));
+            if (!player.isFolded()) {
+                for (Card c : player.getHand()) {
+                    System.out.println("  " + c.getDescription());
+                }
+            }
+        }
+        System.out.println();
+    }
+
+    public void displayWinner(Player player) {
+        System.out.println("The winner is: " + player.getName() + " with " + player.getChips() + " chips!");
+    }
 }
---
+++ poker/src/ui/InputHandler.java
@@ -0,0 +1,16 @@
+package ui;
+
+import java.util.Scanner;
+
+public class InputHandler {
+    private Scanner scanner;
+
+    public InputHandler() {
+        scanner = new Scanner(System.in);
+    }
+
+    public String getCommand() {
+        System.out.print("Command: ");
+        return scanner.nextLine();
+    }
+}
