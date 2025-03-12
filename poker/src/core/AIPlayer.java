07b49bc843e006a31969d88962302c555254d22f
chepelid <denyschepeliuk@gmail.com>
2025-03-12 13:23:54 +0100

    Update AI players logic

--- poker/src/core/AIPlayer.java
+++ poker/src/core/AIPlayer.java
@@ -1,43 +1,41 @@
 package core;
 
+import util.CardUtils;
+
 public class AIPlayer extends Player {
     public AIPlayer(String name, int initialChips, Game game) {
         super(name, initialChips, game);
     }
 
     public void makeDecision() {
-        double randomValue = Math.random();
-        int toCall = game.getHighestBet();
+        int handStrength = CardUtils.evaluateHand(getHand(), game.getTable().getCommunityCards());
+        int toCall = game.getHighestBet() - getBetAmount();
+        double randomFactor = Math.random();
 
-        if (randomValue < 0.1) {
-            fold();
-            return;
-        }
-
-        if (toCall > getBetAmount()){
-            if (randomValue < 0.5) {
-                call();
-            }
-            else {
-                int randomBet = (int) (Math.random() * getChips()/50);
-                if (randomBet == 0)
-                    check();
-
-                bet(toCall - getBetAmount() + randomBet * 50);
-            }
-        }
-        else{
-            if (randomValue < 0.5) {
+        if (handStrength >= 9) { // Weak hands: High Card, Low Pair
+            if (toCall > 0) {
+                if (randomFactor < 0.7) fold(); // Mostly fold if a bet is needed
+                else call(); // Occasionally call
+            } else {
                 check();
             }
-            else {
-                int randomBet = (int) (Math.random() * getChips()/50);
-                if (randomBet == 0)
-                    check();
-
-                bet(toCall - getBetAmount() + randomBet * 50);
+        }
+        else if (handStrength >= 6) { // Medium hands: Two Pair, Three of a Kind, Straight
+            if (toCall > 0) {
+                if (randomFactor < 0.6) call(); // Call most of the time
+                else bet(toCall + 50); // Occasionally raise
+            } else {
+                if (randomFactor < 0.5) check();
+                else bet(50); // Occasionally bet
+            }
+        }
+        else { // Strong hands: Flush, Full House, Four of a Kind, Straight Flush, Royal Flush
+            if (toCall > 0) {
+                if (randomFactor < 0.2) call(); // Occasionally slow play
+                else bet(toCall + (int)(Math.random() * getChips() / 10)); // Bet aggressively
+            } else {
+                bet((int)(Math.random() * getChips() / 8 + 50)); // Aggressive bet
             }
         }
     }
-
 }
